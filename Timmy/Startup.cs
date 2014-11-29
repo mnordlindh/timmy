0e708522de7b1d25d9b28ee4650ede3f548be98d
unknown <mattiasnordlindh@gmail.com>
2014-11-29 21:26:54 +0100

    Initial commit

---
+++ Timmy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Timmy.Startup))]
+namespace Timmy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
